ffa1e01e363b634157e194a88691cfd5fea06241
syd9538 <syd9538@gmail.com>
2022-10-11 20:07:33 +0900

    feat: 5장 람다 - 2

--- src/main/kotlin/CollectionAPI.kt
+++ src/main/kotlin/CollectionAPI.kt
@@ -48,4 +48,51 @@ fun main(args: Array<String>) {
         .filter { it.startsWith("A") }
         .toList())
 
+
+    val naturalNumbers = generateSequence(0){it  + 1 }
+    val numberTo100 = naturalNumbers.takeWhile { it <= 100 }
+    println(numberTo100.sum())
+
+    createAllDoneRunnable().run()
+    println(alphabet())
+}
+
+fun createAllDoneRunnable() : Runnable {
+    return Runnable { println("All done!") }
+}
+
+//fun alphabet() : String {
+//    val result = StringBuilder()
+//    for (letter in 'A' .. 'Z') {
+//        result.append(letter)
+//    }
+//    result.append("\nNot I know the alphabet!")
+//
+//    return result.toString()
+//}
+
+//fun alphabet(): String {
+//    val stringBuilder = StringBuilder()
+//    return with(stringBuilder) {
+//        for(letter in 'A' .. 'Z') {
+//            this.append(letter)
+//    }
+//        append("\n Now I know the alphabet!")
+//        this.toString()
+//    }
+//}
+
+//fun alphabet() = StringBuilder().apply {
+//    for( letter in 'A' .. 'Z') {
+//        append(letter)
+//    }
+//    append("\nNow I know the alphabet!")
+//}.toString()
+
+fun alphabet() = buildString() {
+    for(letter in 'A' .. 'Z') {
+        append(letter)
+    }
+    append("\nNow I now the alphabet!")
+
 }
